a865b3026ebd721948c938cb8d45b957b746bba2
Mingyang0018 <85098418+Mingyang0018@users.noreply.github.com>
2023-12-29 09:27:22 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,26 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import ydata_profiling
+from streamlit_ydata_profiling import st_profile_report
 
-"""
-# Welcome to Streamlit!
+# 设置页面布局
+st.set_page_config(layout="wide")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# 设置标题
+st.title('生成数据分析报告')
+# 设置上传文件组件
+st.subheader('上传CSV')
+uploaded_file = st.file_uploader('')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# 根据是否选择了文件显示消息
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.subheader('数据表')
+    st.write(df)
+    report = ydata_profiling.ProfileReport(df, title='Profile Report', explorative=True)
+    st.subheader('数据分析报告')
+    # st.write(report)
+    st_profile_report(report, navbar=True)
+else:
+    st.write('No file uploaded.')
+    st.info('Upload a csv file.')
